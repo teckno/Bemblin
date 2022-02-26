ab6628d4ec4863ee53393f33531ad6640e3a40f7
Mwape  Bunda Timothy <mwapebt@gmail.com>
2022-02-27 01:44:28 +0200

    static error files

--- backend/Bemblin/settings.py
+++ backend/Bemblin/settings.py
@@ -126,5 +126,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'build', 'static')]
